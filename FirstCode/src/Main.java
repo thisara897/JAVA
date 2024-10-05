5eb7b20bff2c1a6e2dcb1d79990fb2acfe01f4c8
thisara <warshangunasinghe345@gmail.com>
2024-10-05 12:01:42 +0530

    first commitment

---
+++ FirstCode/src/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) throws Exception {
+        car mycar1 = new car();
+        car mycar2 = new car();
+  
+        System.out.println(mycar1.make);
+        System.out.println(mycar1.model);
+        System.out.println();
+        System.out.println(mycar2.make);
+        System.out.println(mycar2.model);
+    }
+}
